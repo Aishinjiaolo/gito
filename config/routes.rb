9810ba179a14ff2edf26cf21b475cf9aa1e28a2d
ktlo <roggerbass@gmail.com>
2013-09-02 00:50:00 +0800

    add introduction slide show at homepage
    twitter bootstrap carouse is not working
    slide change action is not under function call
    jquery failed under rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Gito::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => 'high_voltage/pages#show', :id => 'home'
 end
