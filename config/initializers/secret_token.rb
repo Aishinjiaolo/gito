1f0f389248ed332c2b2deaa56ab9242bc16c5de7
ktlo <roggerbass@gmail.com>
2013-09-01 14:27:34 +0800

    first commit
    generate from bootstrappers v4.2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gito::Application.config.secret_key_base = '47b29b3693aa0a8de3a1aefd9abfb67bf4eea218f7add0d60ec31efc31b2d7c2c66f17ce0e87ce32af8583bb2d0011a62e704dfd4213680cbd5b88dda1ac8dd6'
