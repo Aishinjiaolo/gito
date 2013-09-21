8b54ad38fe0760e41fbc3e1ca24e6d30a9b30ba9
ktlo <roggerbass@gmail.com>
2013-09-21 20:09:29 +0800

    validate username uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  validates :name,
+    :uniqueness => {
+      :case_sensitive => false
+  }
+
   has_many :sheets
 end
